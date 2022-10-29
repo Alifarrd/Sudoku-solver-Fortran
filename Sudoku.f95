3179478d1fb9eb11f68794236f5edb004b452e6d
Ali Mozhdehi Fard <102677728+Alifarrd@users.noreply.github.com>
2022-10-29 14:18:10 +0330

    Vertical move finished

--- Sudoku.f95
+++ Sudoku.f95
@@ -1,10 +1,13 @@
 program Sudoku
-integer::i,j,c
-real,allocatable::k(:,:),ziro(:,:)
+integer::i,j,c,zi,zj
+real,allocatable::k(:,:),ziro(:,:),hk(:),numbers(:)
 OPEN(UNIT=20,FILE='Sudoku.txt',STATUS='old',ACTION='read')
 OPEN(UNIT=40,FILE='Sudoku-ANS.txt',STATUS='replace',ACTION='write')
 
-ALLOCATE (k(9,9))
+ALLOCATE (k(9,9),hk(9),numbers(9))
+do i =1,9
+  numbers(i)=i
+  end do
 !---------------------------------------------------[Read the Sudoku]
 do i=1,9
   do j = 1,9
@@ -21,7 +24,7 @@ do i=1,9
   end do
 end do
 !---------------------------------------------------[Find the ziroes]
-ALLOCATE (ziro(c,11))
+ALLOCATE (ziro(c,12))
 m=1
 do i=1,9
   do j=1,9
@@ -30,15 +33,45 @@ do i=1,9
       ziro(m,2)=j
       do ii=1,9
         ziro(m,ii+2)=ii
+        ziro(m,12)=0
         end do
         m=m+1
  end if
  end do
  end do
-!---------------------------------------------------[Read the Sudoku]
 
+!---------------------------------------------------[vertical Move]
+do ix=1,c
+zi=ziro(ix,1)
+zj=ziro(ix,2)
+hk(:)=k(zi,:) 
+  do n=1,9
+  do j =1,9
+    if (hk(n)==ziro(ix,j+2))then
+      ziro(ix,j+2)=0
+      end if
+      end do
+          end do
+end do
 
+do i=1,c
+ do ii=1,9
+        if (ziro(i,ii+2)/=0)then
+          ziro(i,12)=ziro(i,12)+1
+          end if
+          end do
+          end do
+!---------------------------------------------------[Horizental Move]
 
+  
+  
+  
+  
+  
+  
+  do i=1,c
+  write(40,*)ziro(i,:)
+  end do
 
 
 end 
